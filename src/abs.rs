3166beaee091c61fb050127541d98ff9be3db7dc
sander <sander.wittwer@dengineering.no>
2022-08-19 15:32:28 +0200

    abs: remove unsafe

--- src/abs.rs
+++ src/abs.rs
@@ -8,10 +8,10 @@ use crate::CInt;
 ///
 /// ```
 /// use tinyrlibc::abs;
-/// assert_eq!(unsafe { abs(-2) }, 2);
+/// assert_eq!(abs(-2), 2);
 /// ```
 #[no_mangle]
-pub unsafe extern "C" fn abs(i: CInt) -> CInt {
+pub extern "C" fn abs(i: CInt) -> CInt {
 	i.abs()
 }
 
@@ -21,16 +21,16 @@ mod test {
 
 	#[test]
 	fn neg() {
-		assert_eq!(unsafe { abs(-2) }, 2);
+		assert_eq!(abs(-2), 2);
 	}
 
 	#[test]
 	fn pos() {
-		assert_eq!(unsafe { abs(3) }, 3);
+		assert_eq!(abs(3), 3);
 	}
 
 	#[test]
 	fn zero() {
-		assert_eq!(unsafe { abs(0) }, 0);
+		assert_eq!(abs(0), 0);
 	}
 }
