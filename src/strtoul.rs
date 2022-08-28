8c9be62c171ed3e24f091f65b1db286f3a95e796
Diego Barrios Romero <eldruin@gmail.com>
2022-08-28 13:03:02 +0200

    Code formatting

--- src/strtoul.rs
+++ src/strtoul.rs
@@ -3,7 +3,7 @@
 //! Copyright (c) Jonathan 'theJPster' Pallant 2019
 //! Licensed under the Blue Oak Model Licence 1.0.0
 
-use crate::{CChar, CULong, CStringIter};
+use crate::{CChar, CStringIter, CULong};
 
 /// Rust implementation of C library function `strtoul`.
 ///
@@ -13,56 +13,55 @@ use crate::{CChar, CULong, CStringIter};
 /// function returns zero.
 #[no_mangle]
 pub unsafe extern "C" fn strtoul(s: *const CChar) -> CULong {
-    let mut result: CULong = 0;
-    for c in CStringIter::new(s) {
-        if c >= b'0' && c <= b'9' {
-            result *= 10;
-            result += (c - b'0') as CULong;
-        } else {
-            break;
-        }
-    }
-    result
+	let mut result: CULong = 0;
+	for c in CStringIter::new(s) {
+		if c >= b'0' && c <= b'9' {
+			result *= 10;
+			result += (c - b'0') as CULong;
+		} else {
+			break;
+		}
+	}
+	result
 }
 
 #[cfg(test)]
 mod test {
-    use super::strtoul;
+	use super::strtoul;
 
-    #[test]
-    fn empty() {
-        let result = unsafe { strtoul(b"\0".as_ptr()) };
-        assert_eq!(result, 0);
-    }
+	#[test]
+	fn empty() {
+		let result = unsafe { strtoul(b"\0".as_ptr()) };
+		assert_eq!(result, 0);
+	}
 
-    #[test]
-    fn non_digit() {
-        let result = unsafe { strtoul(b"1234x\0".as_ptr()) };
-        assert_eq!(result, 1234);
-    }
+	#[test]
+	fn non_digit() {
+		let result = unsafe { strtoul(b"1234x\0".as_ptr()) };
+		assert_eq!(result, 1234);
+	}
 
-    #[test]
-    fn bad_number() {
-        let result = unsafe { strtoul(b"x\0".as_ptr()) };
-        assert_eq!(result, 0);
-    }
+	#[test]
+	fn bad_number() {
+		let result = unsafe { strtoul(b"x\0".as_ptr()) };
+		assert_eq!(result, 0);
+	}
 
-    #[test]
-    fn one() {
-        let result = unsafe { strtoul(b"1\0".as_ptr()) };
-        assert_eq!(result, 1);
-    }
+	#[test]
+	fn one() {
+		let result = unsafe { strtoul(b"1\0".as_ptr()) };
+		assert_eq!(result, 1);
+	}
 
-    #[test]
-    fn hundredish() {
-        let result = unsafe { strtoul(b"123\0".as_ptr()) };
-        assert_eq!(result, 123);
-    }
+	#[test]
+	fn hundredish() {
+		let result = unsafe { strtoul(b"123\0".as_ptr()) };
+		assert_eq!(result, 123);
+	}
 
-    #[test]
-    fn big_long() {
-        let result = unsafe { strtoul(b"2147483647\0".as_ptr()) };
-        assert_eq!(result, 2147483647);
-    }
-
+	#[test]
+	fn big_long() {
+		let result = unsafe { strtoul(b"2147483647\0".as_ptr()) };
+		assert_eq!(result, 2147483647);
+	}
 }
